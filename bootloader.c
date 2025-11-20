8165cf17b59e9abca4627f107c13d66087bd21d7
Ghanim Jamil <ghj729@example.com>
2025-11-20 01:12:38 -0600

    Restructured bootloader related files/scripts

--- bootloader.c
+++ bootloader.c
@@ -1,24 +1,65 @@
 #include <stdint.h>
-#include <memory.map.h>
-#include <uart.h>
+#include <hal.h>
+#include <rcc.h>
 
+#define VTOR (*(volatile uint32_t*) 0xE000ED08)
 
 __attribute__((naked, noreturn)) static void start_app(uint32_t pc, uint32_t sp) {
-	__asm(" msr msp, r1 /* load r1 into MSP */\n\
-		bx r0	    /* branch to address at r0 */\n\
-		");
+	__asm volatile (
+			"msr msp, r1\n"
+			"bx r0\n"
+		       );
 }
 
-int main(void) {
-	uart2_init();
-	printf("Bootloader!\r\n");
-	/*uart2_deinit(); should be deinit, but not implemented yet */
+__attribute__((naked, noreturn)) static void init_data(void) {
+	extern uint32_t _sdata, _edata, _sidata, _sbss, _ebss;
+	for (volatile uint32_t* dst = &_sbss; dst < &_ebss; dst++); *dst = 0;
+	for (volatile uint32_t* dst = &_sdata, volatile uint32_t* src = &_sidata; dst < &_edata) *dst++ = *src++;
+}
 
-	uint32_t *app_code = __aprom_start__;
-	uint32_t app_sp = app_code[0];
-	uint32_t app_start = app_code[1];
+static inline void blink_led(void) {
+	uint32_t led_pin = PIN_NUM(5);
+	uint8_t led_port = 'A';
 
-	start_app(app_start, app_sp);
+	gpio_set_mode(led_pin, GPIO_MODE_OUTPUT, led_port);
+
+	size_t i;
+	for (i = 0; i < 5; i++) {
+		gpio_write_pin(led_port, led_pin, GPIO_PIN_SET);
+		delay(500);
+		gpio_write_pin(led_port, led_pin, GPIO_PIN_RESET);
+		delay(500);
+	}
+}
+
+int main() {
+	extern uint32_t _startapp, _startapp_size;
+
+	init_data();
+
+	/* Bootloader stuff */
+	blink_led();
+
+	uint32_t* app_sp = (uint32_t*)_startapp;
+	uint32_t* app_pc = (uint32_t*)(_startapp + 4);
+
+	/* Change our vector table from bootloader to app */
+	uint32_t *curr_vtable = (uint32_t*) &_startapp;
+	uint32_t vtor = VTOR;
+	vtor = (uint32_t) curr_table
+
+	start_app(app_pc, app_sp); /* move bootloader sp to msp, jump to main app */
 
 	for(;;);
 }
+
+
+
+
+
+
+
+
+
+
+
--- startup_f411re.c
+++ startup_f411re.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-uint32_t *vector_table = (uint32_t *) &_stext;
-uint32_t *vtor = (uint32_t *)0xE000ED08;
-*vtor = ((uint32_t) vector_table & 0xFFFFFFF8);
+
+
+int main(void) {
 
