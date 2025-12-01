a72a572703771950fd4deeff37269170d90498dc
Shaccko <ghanimj@hotmail.com>
2025-12-01 00:29:17 -0600

    Didn't add main_raspi.c to git

---
+++ main_raspi.c
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <gpio_raspi.h>
+#include <spi_raspi.h>
+#include <LoRa_raspi.h>
+
+
+int main() {
+
+	gpio_alloc();
+	spidev_init();
+
+	struct lora lora;
+
+	open_spidev();
+		uint8_t status = new_lora(&lora);
+		if (status) {
+			printf("LoRa detected\n");
+		}
+		else {
+			printf("Error\n");
+		}
+
+		usleep(500* 1000);
+	close_spidev();
+	
+	/*
+	 * int fd_bin = open(arg[1], O_RDONLY);
+	 * if (fd_bin < 0) {
+	 * 	perror("Open failed\n");
+	 * 	return -1;
+	 * }
+	 *
+	 * while (contents) {
+	 * 	LoRa_transmit(
+	 */
+
+	return 0;
+}
---
+++ spi_stm32.c
@@ -0,0 +1,68 @@
+#include <stdint.h>
+#include <spi_stm32.h>
+#include <hal.h>
+#include <uart.h>
+
+
+struct spi* spi1 = SPI1;
+
+void spi_init(struct spi* spi, uint32_t spi_pins, uint8_t spi_port) {
+	/* Define SCL baud rate, SPI_CR1
+	 * Select CPOL and CPHA bits for relationship type
+	 * and other stuff
+	 */
+
+	uint8_t spi_af = 5;
+	/* SPI1 config only for now */
+
+	if (spi == SPI1) RCC->APB2ENR |= BIT(12); /* SPI1 Clock */
+
+	gpio_set_mode(spi_pins, GPIO_MODE_AF, spi_port);
+	gpio_set_af(spi_pins, spi_af, spi_port);
+	gpio_set_speed(spi_pins, HIGH_SPEED, spi_port);
+
+
+
+	spi->CR1 = 0;
+	spi->CR2 = 0;
+	spi->CR1 = SPI_MASTER | SPI_8BITMODE | \
+		SPI_CLKMODE0 | SPI_MSBFIRST | SPI_NSS_SOFT | \
+		SPI_BAUDRATE2 | SPI_BIDIMODE0;
+	spi->CR2 = SPI_TXEIE | SPI_RXNEIE; 
+	spi->CR1 |= SPI_ENABLE;
+}
+
+uint32_t spi_receive_byte(struct spi* spi) {
+	while(!(spi->SR & SPI_RXE_FLAG));
+	return spi->DR;
+}
+
+
+
+void spi_write_buf(struct spi* spi, uint8_t* buf) {
+	while(!(spi->SR & SPI_TXE_FLAG)); 
+	spi->DR = (uint32_t)*buf;
+}
+
+void spi_transmit_data(struct spi* spi, uint8_t* buf, size_t len) {
+	while (len--){
+		spi_write_buf(spi, buf++);
+	}
+}
+
+void spi_transmit_receive(struct spi* spi, uint8_t* tx_buf, uint8_t* rx_buf, size_t tx_len) {
+	size_t i = 0;
+	uint8_t dummy[32];
+	if (rx_buf == NULL || rx_buf == 0) rx_buf = dummy;
+	while(tx_len--) {
+		spi_write_buf(spi, tx_buf++);
+		rx_buf[i++] = (uint8_t) spi_receive_byte(spi);
+	}
+}
+
+void spi1_init(void) {
+	uint32_t spi1_pins = SCK1_PIN | MOSI1_PIN | MISO1_PIN | NSS1_PIN;
+	uint8_t spi1_port = SPI1_PORT;
+	spi_init(spi1, spi1_pins, spi1_port);
+}
+
